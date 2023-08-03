9971f939695df9e804856d51cf277e3ddce48336
Rushikesh <rushichoudhari040@gmail.com>
2023-08-04 00:35:07 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,114 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { concatMap, delay, map, mergeMap, of, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'practice1';
+
+  baseUrl =  `https://jsonplaceholder.typicode.com/posts`
+ 
+    of$ = of(1,5,7,9,10,12)
+
+ postwithMap$ = of(1,5,7,9,10,12)
+       .pipe(
+        tap(n => console.log(n)),
+
+        map(n => this._http.get(`${this.baseUrl}/${n}`))
+       )
+
+
+     /////// Concat Map //////
+
+    postwithConcatMap$ = this.of$
+                  .pipe(
+                    tap(n => console.log(`Flat using Concat Map ${n}`)),
+
+                    map(n => {
+                      delay(1000/n)
+                      return n * 3
+                    }),
+                    concatMap(n => this._http.get(`${this.baseUrl}/${n}`))
+                 )
+
+
+    ////// Merge Map ////////
+
+     postwithMergerMAp$ = this.of$
+                 .pipe(
+                  tap(n => console.log(`Flat using Merge Map ${n}`)),
+                  map(n => {
+                    delay(1000/n)
+                    return n * 3
+                  }),
+                  mergeMap(n => this._http.get(`${this.baseUrl}/${n}`))
+                 )
+
+
+   /////// Switch Map //////////
+
+     postwithSwitchMap$ = this.of$
+     .pipe(
+      tap(n => console.log(`Flat using Switch Map ${n}`)),
+      map(n => {
+        delay(1000/n)
+        return n * 2
+      }),
+      switchMap(n =>  this._http.get(`${this.baseUrl}/${n}`))
+     )                     
+
+
+  constructor(private _http : HttpClient){
+
+  }
+
+  ngOnInit(): void {
+      // this.of$
+      // .pipe(
+      //   tap(n => console.log(n)),
+      //   // map(n => `The Value is ${n}`),
+      //   // tap(n => console.log(n))
+      // )
+      //  .subscribe(res => {
+       
+      //   this._http.get(`${this.baseUrl}/${res}`)
+      //   .subscribe(res => {
+      //     console.log(res)
+      //   })
+      //  })
+
+   
+      // this.postwithMap$.subscribe(res => {
+      //   console.log(res)
+      // //  console.log(this.postwithMap$)
+      // res.subscribe(console.log)
+      // })
+
+
+    //  this.postwithConcatMap$
+    //  .subscribe(res => {
+    //   console.log(res)
+    //  })
+
+
+     this.postwithMergerMAp$
+     .subscribe(res => {
+      console.log(res)
+     })
+
+
+    //  this.postwithSwitchMap$
+    //  .subscribe(res => {
+    //   console.log(res)
+    //  })
+
+
+  }
+
 }
+
+
